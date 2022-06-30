cb349b1c2bbdfc1a129bd57b3ded9a61721145b5
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-06-30 18:44:37 -0300

    intento2000

--- api/src/app.js
+++ api/src/app.js
@@ -8,10 +8,13 @@ require('./db.js');
 
 const server = express();
 
+const cors = require('cors')
+
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
@@ -21,7 +24,6 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
 server.use('/', routes);
 
 // Error catching endware.
